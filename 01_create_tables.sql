d6e004fee040ff8c9dd03857eedd46c2f31f024a
YaelQCI <181914@upslp.edu.mx>
2025-10-21 19:14:30 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE usuario(
   id_usuario SERIAL PRIMARY KEY,
   nombre_usuario TEXT NOT NULL
@@ -9,7 +10,7 @@ CREATE TABLE cuenta(
 );
 
 CREATE TABLE proveedor(
-  rfc_proveedor SERIAL PRIMARY KEY,
+  rfc_proveedor TEXT PRIMARY KEY,
   nombre_proveedor TEXT NOT NULL
 );
 
@@ -31,8 +32,7 @@ CREATE TABLE gasto(
   id_usuario INTEGER REFERENCES usuario(id_usuario),
   id_tipo_gasto INTEGER REFERENCES tipo_de_gasto(id_tipo_gasto),
   id_departamento INTEGER REFERENCES departamento(id_departamento),
-  rfc_proveedor INTEGER REFERENCES proveedor(rfc_proveedor),
+  rfc_proveedor TEXT REFERENCES proveedor(rfc_proveedor),
   monto FLOAT NOT NULL,
   fecha DATE NOT NULL
 );  
-
