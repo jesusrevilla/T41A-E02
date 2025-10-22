8acd9386cbfa45e7da06c3951403a66976a294f8
aldogg10 <179862@upslp.edu.mx>
2025-10-21 20:26:07 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -3,7 +3,7 @@
 SELECT 
     d.nombre_departamento,
     SUM(g.monto) AS total_gastado
-FROM gasto g
+FROM gastos g
 JOIN departamento d ON g.id_departamento = d.id_departamento
 GROUP BY d.nombre_departamento
 ORDER BY total_gastado DESC;
