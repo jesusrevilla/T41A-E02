a4428385eda7576a80fab9b0fd698d7dc08d024b
Uriel4443 <182829@upslp.edu.mx>
2025-10-21 19:31:28 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,2 +1,2 @@
 -- Total gastado por cada departamento
-SELECT d.nombre_departamento,g.monto FROM gasto g INNER JOIN departamento d ON g.id_departamento=d.id_departamento;
+SELECT d.nombre_departamento,SUM(g.monto) FROM gasto g INNER JOIN departamento d ON g.id_departamento=d.id_departamento  GROUP BY nombre_departamento;
