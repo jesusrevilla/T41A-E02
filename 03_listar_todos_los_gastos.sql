cc5013390e92c3a87ce4e1ccb831aa8a467a90a9
daf01-web <171513@upslp.edu.mx>
2025-10-21 19:10:37 -0600

    Update 03_listar_todos_los_gastos.sql

--- 03_listar_todos_los_gastos.sql
+++ 03_listar_todos_los_gastos.sql
@@ -18,7 +18,8 @@ SELECT
 FROM gasto g
 JOIN usuario u ON g.id_usuario = u.id_usuario
 JOIN departamento d ON g.id_departamento = d.id_departamento
-JOIN tipo_gasto tg ON g.id_tipo_gasto = tg.id_tipo_gasto
+JOIN tipo_de_gasto tg ON g.id_tipo_gasto = tg.id_tipo_gasto
 JOIN cuenta c ON tg.id_cuenta = c.id_cuenta
 JOIN proveedor p ON g.rfc_proveedor = p.rfc_proveedor
 ORDER BY g.fecha, g.id_gasto;
+
