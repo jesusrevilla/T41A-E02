bea3a83ad8232e5bc7948bfbdaf0ca5aab147e48
AngelSaucedo-dev <ange1eduard08122003@gmail.com>
2025-10-21 19:25:48 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,7 +1,8 @@
 -- Total de gastos por día
-SELECT
+SELECT 
     g.fecha,
-    SUM(g.monto) AS total_gastos_dia
+    COUNT(g.id_gasto) AS cantidad_gastos,
+    SUM(g.monto) AS total_por_dia
 FROM gasto g
 GROUP BY g.fecha
 ORDER BY g.fecha;
