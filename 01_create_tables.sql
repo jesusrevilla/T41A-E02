4db65a2fd5ac478b6897d7dc3c419f74fd0eef60
m00n07 <174653@upslp.edu.mx>
2025-10-21 19:14:55 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,42 @@
+-- create
+CREATE TABLE usuario (
+  id_usuario INT PRIMARY KEY,
+  nombre_usuario VARCHAR(100)
+);
 
+CREATE TABLE departamento (
+  id_departamento INT PRIMARY KEY,
+  nombre_departamento VARCHAR(100)
+);
+
+CREATE TABLE cuenta (
+  id_cuenta INT PRIMARY KEY,
+  nombre_cuenta VARCHAR(100)
+);
+
+CREATE TABLE tipo_de_gasto (
+  id_tipo_gasto INT PRIMARY KEY,
+  nombre_tipo_gasto VARCHAR(100),
+  id_cuenta INT,
+  FOREIGN KEY (id_cuenta) REFERENCES cuenta(id_cuenta)
+);
+
+CREATE TABLE proveedor (
+  rfc_proveedor VARCHAR(13) PRIMARY KEY,
+  nombre_proveedor VARCHAR(100)
+);
+
+
+CREATE TABLE gasto (
+  id_gasto INT PRIMARY KEY,
+  id_usuario INT,
+  id_tipo_gasto INT,
+  id_departamento INT,
+  rfc_proveedor VARCHAR(13),
+  monto DECIMAL(10, 2),
+  fecha DATE,
+  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+  FOREIGN KEY (id_tipo_gasto) REFERENCES tipo_de_gasto(id_tipo_gasto),
+  FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento),
+  FOREIGN KEY (rfc_proveedor) REFERENCES proveedor(rfc_proveedor)
+);
