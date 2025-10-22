f721685c8eec924882afbeb3110b35995ef11ff2
Cristphertorres21 <cristophertorresdza@gmail.com>
2025-10-21 19:07:34 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,1 +1,11 @@
--- Total gastado por cada departamento
+SELECT
+    D.nombre_departamento,
+    SUM(G.monto) AS total_gastado
+FROM
+    gasto G
+JOIN
+    departamento D ON G.id_departamento = D.id_departamento
+GROUP BY
+    D.nombre_departamento
+ORDER BY
+    total_gastado DESC;
