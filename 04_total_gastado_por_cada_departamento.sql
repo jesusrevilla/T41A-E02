60e58fce9bee4dee9611bf9dbe1071a0ee677767
JuanpiMarinD <126593527+JuanpiMarinD@users.noreply.github.com>
2025-10-21 19:14:58 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,1 +1,7 @@
 -- Total gastado por cada departamento
+SELECT d.nombre_departamento,
+    COALESCE(SUM(g.monto), 0) AS total_gastado
+FROM departamento d
+LEFT JOIN
+    gasto g ON d.id_departamento = g.id_departamento
+GROUP BY d.nombre_departamento;
