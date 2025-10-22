1ce7e8a9e308d7bc0251504cc5b842a364291762
Uriel4443 <182829@upslp.edu.mx>
2025-10-21 19:19:20 -0600

    Update 03_listar_todos_los_gastos.sql

--- 03_listar_todos_los_gastos.sql
+++ 03_listar_todos_los_gastos.sql
@@ -1,1 +1,3 @@
 -- Listar todos los gastos con información del usuario, tipo de gasto y proveedor
+SELECT u.id_usuario,u.nombre_usuario,t.nombre_tipo_gasto,p.nombre_proveedor FROM gasto g INNER JOIN usuario u ON g.id_usuario=u.id_usuario
+INNER JOIN tipo_de_gasto t ON g.id_tipo_gasto=t.id_tipo_gasto INNER JOIN proveedor p ON g.rfc_proveedor=p.rfc_proveedor;
