aea830e6cc2ada2edf4f903ca5c9e05fe9a836ac
YaelQCI <181914@upslp.edu.mx>
2025-10-21 19:45:04 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,7 +1,13 @@
 
+
 CREATE TABLE usuario(
   id_usuario SERIAL PRIMARY KEY,
   nombre_usuario TEXT NOT NULL
+);
+
+CREATE TABLE departamento(
+  id_departamento SERIAL PRIMARY KEY,
+  nombre_departamento TEXT NOT NULL
 );
 
 CREATE TABLE cuenta(
@@ -9,30 +15,23 @@ CREATE TABLE cuenta(
   nombre_cuenta TEXT NOT NULL
 );
 
-CREATE TABLE proveedor(
-  rfc_proveedor TEXT PRIMARY KEY,
-  nombre_proveedor TEXT NOT NULL
-);
-
-
-CREATE TABLE departamento(
-  id_departamento SERIAL PRIMARY KEY,
-  nombre_departamento TEXT NOT NULL
-);
-
-
 CREATE TABLE tipo_de_gasto(
   id_tipo_gasto SERIAL PRIMARY KEY,
   nombre_tipo_gasto TEXT NOT NULL,
-  id_cuenta INTEGER REFERENCES cuenta(id_cuenta)
+  id_cuenta INT REFERENCES cuenta(id_cuenta)
 );
+
+CREATE TABLE proveedor(
+  rfc_proveedor TEXT PRIMARY KEY,
+  nombre_proveedor TEXT NOT NULL
+); 
 
 CREATE TABLE gasto(
   id_gasto SERIAL PRIMARY KEY,
-  id_usuario INTEGER REFERENCES usuario(id_usuario),
-  id_tipo_gasto INTEGER REFERENCES tipo_de_gasto(id_tipo_gasto),
-  id_departamento INTEGER REFERENCES departamento(id_departamento),
+  id_usuario INT REFERENCES usuario(id_usuario),
+  id_tipo_gasto INT REFERENCES tipo_de_gasto(id_tipo_gasto),
+  id_departamento INT REFERENCES departamento(id_departamento),
   rfc_proveedor TEXT REFERENCES proveedor(rfc_proveedor),
-  monto FLOAT NOT NULL,
+  monto DECIMAL(8,2) NOT NULL,
   fecha DATE NOT NULL
-);  
+);
