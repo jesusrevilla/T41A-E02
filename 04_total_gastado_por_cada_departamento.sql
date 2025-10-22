911dd44dd7c0939d22b29e5677e91d57bb698c99
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-10-21 19:22:54 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,1 +1,6 @@
 -- Total gastado por cada departamento
+select dep.nombre_departamento, sum(gs.monto) as total_gastado
+  from gasto gs
+  join departamento dep
+  on gs.id_departamento = dep.id_departamento
+  group by dep.nombre_departamento;
