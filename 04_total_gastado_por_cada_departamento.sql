1bc3a3b1709718182e88cd7bf500fe09d4a8e1f6
AngelCasti <125672277+AngelCasti@users.noreply.github.com>
2025-10-21 19:37:55 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,2 +1,2 @@
 -- Total gastado por cada departamento
-SELECT gasto.id_departamento, depa.nombre_departamento, gasto.monto FROM departamento depa JOIN gasto ON depa.id_departamento = gasto.id_departamento;
+SELECT gasto.id_departamento, gasto.monto FROM departamento depa JOIN gasto ON depa.id_departamento = gasto.id_departamento;
