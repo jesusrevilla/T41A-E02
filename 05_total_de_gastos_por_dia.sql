4599b1d66c657192ed544e8bb82a0d3c13b2322e
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-21 22:19:51 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,11 +1,1 @@
--- Total de gastos por día
-SELECT
-    g.fecha,
-    COUNT(g.id_gasto) AS cantidad_gastos,
-    SUM(g.monto) AS total_gastado
-FROM
-    gasto g
-GROUP BY
-    g.fecha
-ORDER BY
-    g.fecha;
+
