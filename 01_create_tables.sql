eefa520d00b015865dd2446bcad9462b1a6aef4d
daf01-web <171513@upslp.edu.mx>
2025-10-21 19:10:11 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -2,6 +2,7 @@
 -- Crear las tablas
 -- Usar las 3FN (Normalización) en tablas: usuario, departamento, cuenta, tipo_gasto, proveedor, gasto
 
+
 CREATE TABLE departamento (
     id_departamento SERIAL PRIMARY KEY,
     nombre_departamento VARCHAR(100) NOT NULL UNIQUE
@@ -21,7 +22,7 @@ CREATE TABLE cuenta (
 );
 
 
-CREATE TABLE tipo_gasto (
+CREATE TABLE tipo_de_gasto (
     id_tipo_gasto SERIAL PRIMARY KEY,
     nombre_tipo_gasto VARCHAR(150) NOT NULL,
     id_cuenta INT NOT NULL REFERENCES cuenta(id_cuenta),
@@ -38,9 +39,10 @@ CREATE TABLE proveedor (
 CREATE TABLE gasto (
     id_gasto SERIAL PRIMARY KEY,
     id_usuario INT NOT NULL REFERENCES usuario(id_usuario),
-    id_tipo_gasto INT NOT NULL REFERENCES tipo_gasto(id_tipo_gasto),
+    id_tipo_gasto INT NOT NULL REFERENCES tipo_de_gasto(id_tipo_gasto),
     id_departamento INT NOT NULL REFERENCES departamento(id_departamento),
     rfc_proveedor VARCHAR(20) NOT NULL REFERENCES proveedor(rfc_proveedor),
     monto NUMERIC(12,2) NOT NULL CHECK (monto >= 0),
     fecha DATE NOT NULL
 );
+
