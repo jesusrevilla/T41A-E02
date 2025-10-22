e4670c0a3e6fe271e2c956ad9d4f12e95ec11dc6
Cristphertorres21 <cristophertorresdza@gmail.com>
2025-10-21 19:10:57 -0600

    Update 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql

--- 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
+++ 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
@@ -1,6 +1,2 @@
-SELECT
-    UPPER(nombre_usuario) AS nombre_usuario_mayusculas
-FROM
-    usuario
-ORDER BY
-    id_usuario;
+SELECT UPPER(nombre_usuario) AS nombre_usuario_mayusculas
+FROM usuario;
