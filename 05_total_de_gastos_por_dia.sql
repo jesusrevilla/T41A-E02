afd7ea4d6a9903cbd061ad3df4a7dfd48259d9d8
andreupslpornelas <182085@upslp.edu.mx>
2025-10-21 19:05:26 -0600

    Feature 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,1 +1,11 @@
--- Total de gastos por día
+-- 05_total_de_gastos_por_dia.sql
+-- Update 05_total_de_gastos_por_dia.sql
+SELECT
+    g.fecha,
+    SUM(g.monto) AS total_gastado_por_dia
+FROM
+    gasto g
+GROUP BY
+    g.fecha
+ORDER BY
+    g.fecha DESC;
