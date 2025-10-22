642e1e1e5befcd49abb5d51dc8f6daf942efdedf
daf01-web <171513@upslp.edu.mx>
2025-10-21 19:28:09 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,9 +1,8 @@
 -- Total gastado por cada departamento
 
-
 SELECT
   d.nombre_departamento,
-  COALESCE(SUM(g.monto),0) AS total_gastado
+  COALESCE(SUM(g.monto), 0) AS total_gastado
 FROM departamento d
 LEFT JOIN gasto g ON g.id_departamento = d.id_departamento
 GROUP BY d.nombre_departamento
