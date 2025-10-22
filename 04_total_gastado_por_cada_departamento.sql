650f43cf9a1852f1f342e7b9c75ca929dec50d55
Fano-Rdz <178584@upslp.edu.mx>
2025-10-21 19:41:03 -0600

    Change to LEFT JOIN for total spent by department

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,12 +1,7 @@
 -- Total gastado por cada departamento
-SELECT 
-  d.nombre_departamento,
-  SUM(g.monto) AS total_gastado
-FROM 
-  gasto g
-JOIN 
-  departamento d ON g.id_departamento = d.id_departamento
-GROUP BY 
-  d.nombre_departamento
-ORDER BY 
-  total_gastado DESC;
+SELECT d.nombre_departamento,
+    COALESCE(SUM(g.monto), 0) AS total_gastado
+FROM departamento d
+LEFT JOIN
+    gasto g ON d.id_departamento = g.id_departamento
+GROUP BY d.nombre_departamento;
