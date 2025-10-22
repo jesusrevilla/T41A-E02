f01cdfc8dca4a41f8ac9fdca44fbf373d5b05991
m00n07 <174653@upslp.edu.mx>
2025-10-21 19:24:44 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,1 +1,7 @@
 -- Total de gastos por día
+SELECT g.fecha::date AS fecha,
+       COUNT(*) AS cantidad_gastos,
+       COALESCE(SUM(g.monto), 0) AS total_del_dia
+FROM gasto g
+GROUP BY g.fecha::date
+ORDER BY g.fecha::date;
