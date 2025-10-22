672c421eef5b501c3ecef80a6d043b2fd41fe2a7
Cristian R. Godinez Limones <cristianlimones3@gmail.com>
2025-10-21 19:05:25 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,1 +1,7 @@
 -- Total gastado por cada departamento
+SELECT d.nombre_departamento AS departamento,
+       COALESCE(SUM(g.monto), 0) AS total_gastado
+FROM departamento d
+LEFT JOIN gasto g ON g.id_departamento = d.id_departamento
+GROUP BY d.id_departamento, d.nombre_departamento
+ORDER BY d.nombre_departamento;
