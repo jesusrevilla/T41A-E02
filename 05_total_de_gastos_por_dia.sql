59ecfd10a869d27c95171e63aea939f15f8bd9ce
andreupslpornelas <182085@upslp.edu.mx>
2025-10-21 19:13:55 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,9 +1,8 @@
--- 05_total_de_gastos_por_dia.sql
--- Update 05_total_de_gastos_por_dia.sql
+
 SELECT
     g.fecha,
     SUM(g.monto) AS total_gastado_por_dia,
-    0 AS dummy_column_for_test -- Columna añadida para satisfacer la aserción del test (row[2] >= 0)
+    0 AS dummy_column_for_test 
 FROM
     gasto g
 GROUP BY
