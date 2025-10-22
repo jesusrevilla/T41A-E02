ed320163bfc1d2c0c58de0397886cb03a0624e3e
Javierxd1383 <javiercruzz1383@gmail.com>
2025-10-21 19:58:43 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,38 +1,40 @@
-CREATE TABLE usuario(
-  id_usuario SERIAL PRIMARY KEY,
-  nombre_usuario VARCHAR(100) NOT NULL
+CREATE TABLE usuario (
+    id_usuario SERIAL PRIMARY KEY,
+    nombre_usuario VARCHAR(100) NOT NULL
 );
 
-
-
-CREATE TABLE departamento(
-  id_departamento SERIAL PRIMARY KEY,
-  nombre_departamento TEXT NOT NULL
+CREATE TABLE departamento (
+    id_departamento SERIAL PRIMARY KEY,
+    nombre_departamento VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE cuenta(
- id_cuenta SERIAL PRIMARY KEY,
-  nombre_cuenta TEXT NOT NULL
+CREATE TABLE cuenta (
+    id_cuenta SERIAL PRIMARY KEY,
+    nombre_cuenta VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE tipo_de_gasto(
-  id_tipo_gasto SERIAL PRIMARY KEY,
-  nombre_tipo_gasto TEXT NOT NULL,
-  id_cuenta INTEGER REFERENCES cuenta(id_cuenta) NOT NULL
+CREATE TABLE tipo_de_gasto (
+    id_tipo_gasto SERIAL PRIMARY KEY,
+    nombre_tipo_gasto VARCHAR(100) NOT NULL,
+    id_cuenta INT NOT NULL,
+    FOREIGN KEY (id_cuenta) REFERENCES cuenta(id_cuenta)
 );
 
-
-CREATE TABLE proveedor(
-  rfc_proveedor VARCHAR(20) PRIMARY KEY,
-  nombre_proveedor TEXT NOT NULL
+CREATE TABLE proveedor (
+    rfc_proveedor VARCHAR(13) PRIMARY KEY,
+    nombre_proveedor VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE gasto(
-  id_gasto SERIAL PRIMARY KEY,
-  id_usuario INTEGER REFERENCES usuario(id_usuario),
-  id_tipo_gasto INTEGER REFERENCES tipo_de_gasto  (id_tipo_gasto),
-  id_departamento INTEGER REFERENCES departamento(id_departamento),
-  rfc_proveedor VARCHAR(20) REFERENCES proveedor(rfc_proveedor),
-  monto NUMERIC (10,4) NOT NULL,
-  fecha DATE NOT NULL
+CREATE TABLE gasto (
+    id_gasto SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    id_tipo_gasto INT NOT NULL,
+    id_departamento INT NOT NULL,
+    rfc_proveedor VARCHAR(13) NOT NULL,
+    monto NUMERIC(10,2) NOT NULL,
+    fecha DATE NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (id_tipo_gasto) REFERENCES tipo_de_gasto(id_tipo_gasto),
+    FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento),
+    FOREIGN KEY (rfc_proveedor) REFERENCES proveedor(rfc_proveedor)
 );
