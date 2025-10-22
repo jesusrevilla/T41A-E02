04d8876d5ea8b228592383272747f4510987e8f4
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-21 22:26:10 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,1 +1,8 @@
-
+-- Total gastado por cada departamento
+SELECT 
+    d.nombre_departamento,
+    COUNT(g.id_gasto) as total_gastos,
+    SUM(g.monto) as monto_total
+FROM departamento d
+    LEFT JOIN gasto g ON d.id_departamento = g.id_departamento
+GROUP BY d.id_departamento, d.nombre_departamento;
