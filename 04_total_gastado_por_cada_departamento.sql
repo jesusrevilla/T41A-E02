63ec3e07edde76d2ab6a2e11ab28e4183810fb57
Diego0086 <178218@upslp.edu.mx>
2025-10-21 19:38:56 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,2 +1,2 @@
 -- Total gastado por cada departamento
-SELECT id_departamento, SUM(monto) FROM gastos GROUP BY id_departamento;
+SELECT id_departamento, SUM(monto) FROM gasto GROUP BY id_departamento;
