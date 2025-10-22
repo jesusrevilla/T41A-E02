a789629abf8a3bcf209c0f18f0cb270913d6119f
andreupslpornelas <182085@upslp.edu.mx>
2025-10-21 19:17:17 -0600

    Update 06_buscar_proveedores_cuyo_nombre_contiene_tech.sql

--- 06_buscar_proveedores_cuyo_nombre_contiene_tech.sql
+++ 06_buscar_proveedores_cuyo_nombre_contiene_tech.sql
@@ -1,5 +1,4 @@
--- 06_buscar_proveedores_cuyo_nombre_contiene_tech.sql
--- Add comment to SQL file for clarity. Buscar proveedores cuyo nombre contenga 'tech'
+
 SELECT
     p.rfc_proveedor,
     p.nombre_proveedor
@@ -7,4 +6,3 @@ FROM
     proveedor p
 WHERE
     p.nombre_proveedor ILIKE '%tech%';
--- Si no se usa PostgreSQL, usar: WHERE UPPER(p.nombre_proveedor) LIKE UPPER('%tech%');
