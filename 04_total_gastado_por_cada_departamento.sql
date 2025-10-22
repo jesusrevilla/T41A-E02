532f2be5910d5ab60d1f3123bcec161dcbe81f1f
aldogg10 <179862@upslp.edu.mx>
2025-10-21 19:34:23 -0600

    Update 04_total_gastado_por_cada_departamento.sql

--- 04_total_gastado_por_cada_departamento.sql
+++ 04_total_gastado_por_cada_departamento.sql
@@ -1,1 +1,9 @@
 -- Total gastado por cada departamento
+
+SELECT
+    c.nombre_cuenta,
+    SUM(g.monto) AS total_gastado
+FROM gasto g
+JOIN tipo_de_gasto tg ON g.id_tipo_gasto = tg.id_tipo_gasto
+JOIN cuenta c ON tg.id_cuenta = c.id_cuenta
+GROUP BY c.nombre_cuenta;
