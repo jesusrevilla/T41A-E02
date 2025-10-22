5671cb1444fb631bf487eab8885cdfebbe9ada23
Uriel4443 <182829@upslp.edu.mx>
2025-10-21 19:38:06 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,2 +1,2 @@
 -- Total de gastos por día
-SELECT fecha,SUM(monto) FROM gasto GROUP BY fecha ORDER BY fecha;
+SELECT d.nombre_departamento,g.id_gasto,SUM(g.monto) FROM gasto g INNER JOIN departamento d ON g.id_departamento=d.id_departamento  GROUP BY nombre_departamento,id_gasto;
