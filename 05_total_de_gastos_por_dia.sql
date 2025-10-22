8ea93ec88a590d1a886703bb4657dc0a8774c7bd
menaxixi <179800@upslp.edu.mx>
2025-10-21 19:42:22 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,5 +1,7 @@
 -- Total de gastos por día
-SELECT g.fecha, SUM(g.monto) AS total_gastado
-  FROM gasto g
-  GROUP BY g.fecha
-  ORDER BY g.fecha;
+SELECT g.fecha,
+       SUM(g.monto) AS total_gastado,
+       SUM(g.monto) AS total_columna_extra
+FROM gasto g
+GROUP BY g.fecha
+ORDER BY g.fecha;
