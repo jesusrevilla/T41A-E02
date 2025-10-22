eced17c3a7d3e088e1522ea4a36f8e07053fddb2
Juan Pablo Csnv <175329@upslp.edu.mx>
2025-10-21 19:39:42 -0600

    01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,39 +1,39 @@
+CREATE TABLE cuenta (
+    id_cuenta INTEGER PRIMARY KEY,
+    nombre_cuenta VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE usuario (
-  id_usuario INT PRIMARY KEY,
-  nombre_usuario VARCHAR(50) NOT NULL
+    id_usuario INTEGER PRIMARY KEY,
+    nombre_usuario VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE departamento(
-  id_departamento INT PRIMARY KEY,
-  nombre_departamento VARCHAR(50) NOT NULL
+CREATE TABLE departamento (
+    id_departamento INTEGER PRIMARY KEY,
+    nombre_departamento VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE cuenta(
-  id_cunta INT PRIMARY KEY,
-  nombre_cuenta VARCHAR(50) NOT NULL,
+CREATE TABLE proveedor (
+    rfc_proveedor VARCHAR(13) PRIMARY KEY,
+    nombre_proveedor VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE tipo_de_gasto(
-  id_tipo_gasto INT PRIMARY KEY,
-  nombre_tipo_gasto VARCHAR(50) NOT NULL
-  id_cuenta INT NOT NULL,
-  FOREIGN KEY (id_cuenta) REFERENCES cuenta(id_cuenta)
-);
-
-CREATE TABLE proveedor(
-  rfc_porveedor VARCHAR(13) PRIMARY KEY, 
-  nombre_proveedor VARCHAR(100) NOT NULL
+CREATE TABLE tipo_de_gasto (
+    id_tipo_gasto INTEGER PRIMARY KEY,
+    nombre_tipo_gasto VARCHAR(100) NOT NULL,
+    id_cuenta INTEGER NOT NULL,
+    FOREIGN KEY (id_cuenta) REFERENCES cuenta(id_cuenta)
 );
 
 CREATE TABLE gasto (
-    id_gasto INT PRIMARY KEY,
-    monto DECIMAL(10, 2) NOT NULL,
-    fecha DATE NOT NULL,
-    id_usuario INT NOT NULL,
-    id_tipo_gasto INT NOT NULL,
-    id_departamento INT NOT NULL,
+    id_gasto INTEGER PRIMARY KEY,
+    id_usuario INTEGER NOT NULL,
+    id_tipo_gasto INTEGER NOT NULL,
+    id_departamento INTEGER NOT NULL,
     rfc_proveedor VARCHAR(13) NOT NULL,
-    
+    monto NUMERIC(10, 2) NOT NULL,
+    fecha DATE NOT NULL,
+
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
     FOREIGN KEY (id_tipo_gasto) REFERENCES tipo_de_gasto(id_tipo_gasto),
     FOREIGN KEY (id_departamento) REFERENCES departamento(id_departamento),
