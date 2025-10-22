74183bb3457cc9c7aa05951ea1479d6c6df18883
EdgarErh <160251219+EdgarErh@users.noreply.github.com>
2025-10-21 19:37:09 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,9 +1,3 @@
 -- Total de gastos por día
-SELECT fecha AS dia,
-    SUM(monto) AS Total_diario
-FROM
-    gasto
-GROUP BY
-    fecha
-ORDER BY
-    fecha;
+SELECT fecha AS Día, SUM(monto) AS Ttal_diario
+FROM gasto GROUP BY fecha ORDER BY fecha;
