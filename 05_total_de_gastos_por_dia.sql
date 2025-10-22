d94017ec74c53641db8a487d8c914c4c9c17a2d5
Cruz <183213@upslp.edu.mx>
2025-10-21 19:43:18 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,5 +1,7 @@
 -- Total de gastos por día
-SELECT ga.fecha, SUM(g.monto) AS Total_Por_Dia
+SELECT ga.fecha, SUM(g.monto) AS Total_Dia
 FROM gasto g
 JOIN gasto ga ON ga.fecha =  g.fecha
 GROUP BY ga.fecha;
+
+
