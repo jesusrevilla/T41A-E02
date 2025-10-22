400b1789d786cbec4608cbab4a1702c8e186a9a9
AdriannMo <165124296+AdriannMo@users.noreply.github.com>
2025-10-21 19:23:38 -0600

    Update 05_total_de_gastos_por_dia.sql

--- 05_total_de_gastos_por_dia.sql
+++ 05_total_de_gastos_por_dia.sql
@@ -1,1 +1,10 @@
 -- Total de gastos por día
+-- Update 05_total_de_gastos_por_dia.sql
+-- Esta consulta muestra el número de gastos y el monto total por día.
+SELECT
+    fecha,
+    COUNT(id_gasto) AS numero_de_gastos,
+    SUM(monto) AS total_diario
+FROM gasto
+GROUP BY fecha
+ORDER BY fecha;
