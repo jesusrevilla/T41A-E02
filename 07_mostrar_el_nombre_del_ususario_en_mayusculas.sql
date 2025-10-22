7cdd752cc82e3378a39b4ce6f81f733730cc14cc
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-10-21 19:22:48 -0600

    Update 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql

--- 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
+++ 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
@@ -1,1 +1,8 @@
 -- Mostrar nombre de usuario en mayúsculas
+-- Mostrar todos los usuarios con nombre en mayúsculas
+SELECT 
+    id_usuario,
+    UPPER(nombre_usuario) AS nombre_usuario_mayusculas,
+    id_departamento
+FROM usuario
+ORDER BY nombre_usuario_mayusculas;
