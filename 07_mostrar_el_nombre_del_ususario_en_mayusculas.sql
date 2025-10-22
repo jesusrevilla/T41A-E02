c485d77fedd3f303540fa71cda4e91c542438341
Yovana-Palencia <174197@upslp.edu.mx>
2025-10-21 22:41:40 -0600

    Update 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql

--- 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
+++ 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
@@ -1,8 +1,4 @@
--- Mostrar nombre de usuario en mayúsculas
--- Mostrar todos los usuarios con nombre en mayúsculas
--- Mostrar el nombre del usuario en mayúsculas
-
 SELECT 
-    id_usuario,
     UPPER(nombre_usuario) AS nombre_usuario_mayusculas
 FROM usuario;
+
