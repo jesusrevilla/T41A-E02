cc7fe825b5da3ccbf71ccca5df6da6a6e6618c1d
David Vidales <vidalesrmzdavid@gmail.com>
2025-10-21 19:38:51 -0600

    Update 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql

--- 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
+++ 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
@@ -1,6 +1,7 @@
 -- Mostrar nombre de usuario en mayúsculas
 SELECT 
-  id_usuario,
-  UPPER(nombre_usuario) AS nombre_en_mayusculas
+  UPPER(nombre_usuario) AS nombre_en_mayusculas,
+  id_usuario
 FROM usuario;
+
 
