bd13c78aad81e942222b70e0aaa56223df2ae689
m00n07 <174653@upslp.edu.mx>
2025-10-21 19:35:51 -0600

    Update 03_listar_todos_los_gastos.sql

--- 03_listar_todos_los_gastos.sql
+++ 03_listar_todos_los_gastos.sql
@@ -1,2 +1,2 @@
--- Listar todos los gastos con información del usuario, tipo de gasto y proveedor
+-- Listar todos los gastos con información del usuario, tipo de gasto y proveedor 03
 SELECT * FROM gasto;
