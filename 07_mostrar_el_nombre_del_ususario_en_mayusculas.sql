9f08c4407f405e1d82d47f1f27afdf4adba4d615
aldogg10 <179862@upslp.edu.mx>
2025-10-21 19:50:14 -0600

    Update 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql

--- 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
+++ 07_mostrar_el_nombre_del_ususario_en_mayusculas.sql
@@ -1,9 +1,6 @@
 -- Mostrar nombre de usuario en mayúsculas
 
-SELECT
-    p.nombre_proveedor,
-    g.monto,
-    g.fecha
-FROM gasto g
-JOIN proveedor p ON g.rfc_proveedor = p.rfc_proveedor
-ORDER BY g.fecha DESC;
+SELECT 
+    id_usuario,
+    UPPER(nombre_usuario) AS nombre_usuario_mayusculas
+FROM usuario;
