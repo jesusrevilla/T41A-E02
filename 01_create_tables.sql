a0c0c4bdde94df369b81b97c68bf9050495d06e3
aldogg10 <179862@upslp.edu.mx>
2025-10-21 19:45:01 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,23 +1,24 @@
-CREATE TABLE departamento (
-    id_departamento int PRIMARY KEY,
-    nombre_departamento varchar(40) NOT NULL
-);
 CREATE TABLE usuario (
-    id_usuario int PRIMARY KEY,
+    id_usuario SERIAL PRIMARY KEY,
     nombre_usuario varchar(30) NOT NULL,
-    id_departamento int,
-    FOREIGN KEY (id_departamento) references departamento(id_departamento)
+);
+
+CREATE TABLE departamento (
+    id_departamento SERIAL PRIMARY KEY,
+    nombre_departamento varchar(40) NOT NULL
 );
  
 CREATE TABLE cuenta (
-    id_cuenta int PRIMARY KEY,
+    id_cuenta SERIAL PRIMARY KEY,
     nombre_cuenta varchar(30) NOT NULL
 );
  
 CREATE TABLE tipo_de_gasto (
-    id_tipo_gasto int PRIMARY KEY,
+    id_tipo_gasto SERIAL PRIMARY KEY,
     nombre_tipo_gasto varchar(30) NOT NULL,
     id_cuenta int,
+    rfc_proveedor varchar(20),
+    nombre_proveedor VARCHAR(40) NOT NULL
     FOREIGN KEY (id_cuenta) references cuenta(id_cuenta)
 );
  
